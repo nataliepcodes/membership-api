69d35e0ca1f70e4e7e815f1e65765439983ba0df
npeyre <90184564+npeyre@users.noreply.github.com>
2024-10-21 23:18:15 +0100

    Added additional routes

--- app.py
+++ app.py
@@ -2,9 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/members', methods=['GET'])
+@app.route('/member', methods=['GET'])
 def get_members():
     return '<h1>This route returns all the members</h1>'
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    return '<h1>This route returns one member</h1>'
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    return '<h1>This route adds a new member</h1>'
+
+@app.route('/member/<int:member_id>', methods=['PUT', 'PATCH'])
+def edit_member(member_id):
+    return '<h1>This route updates a member by ID</h1>'
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    return '<h1>This route removes member ID</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
