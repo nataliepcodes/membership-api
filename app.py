1371ab39466983e40d5bc5dbceea9c580af4db4b
npeyre <90184564+npeyre@users.noreply.github.com>
2024-10-24 23:42:46 +0100

    Marked todo sections

--- app.py
+++ app.py
@@ -15,13 +15,16 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+# TODO 
 @app.route('/member', methods=['GET'])
 def get_members():
     return '<h1>This route returns all the members</h1>'
 
+# TODO 
 @app.route('/member/<int:member_id>', methods=['GET'])
 def get_member(member_id):
     return '<h1>This route returns one member</h1>'
+
 
 @app.route('/member', methods=['POST'])
 def add_member():
@@ -33,13 +36,18 @@ def add_member():
     # test for Postman return: <h1>The name is Alice, the email is alice@whatever.com, and the level is Gold</h1>
     return '<h1>The name is {}, the email is {}, and the level is {}</h1>'.format(name, email, level)
 
+
+# TODO 
 @app.route('/member/<int:member_id>', methods=['PUT', 'PATCH'])
 def edit_member(member_id):
     return '<h1>This route updates a member by ID</h1>'
 
+
+# TODO 
 @app.route('/member/<int:member_id>', methods=['DELETE'])
 def delete_member(member_id):
     return '<h1>This route removes member by ID</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
